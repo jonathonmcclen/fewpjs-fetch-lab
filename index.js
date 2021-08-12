0abfe2fdf4e34d2b71afcf85022efb6fb6912955
--replace-all <jonathonsmcclendon@gmail.com>
2021-08-11 20:33:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json){
+    return json
+  })
 }
 
 function renderBooks(books) {
@@ -13,5 +19,6 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+  let books = fetchBooks();
+  renderBooks(books)
 });
